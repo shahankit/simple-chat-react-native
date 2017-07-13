13043732a6d10d2c7dbad6778de2a3a78c02237a
Ankit Shah <shahankit2313@gmail.com>
2017-07-13 19:42:20 +0530

    Added sqlite populatedb code

--- index.ios.js
+++ index.ios.js
@@ -9,9 +9,16 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
+import loremIpsum from 'lorem-ipsum-react-native';
+import uuid from 'uuid/v4';
+
+SQLite.enablePromise(true);
+
+const NUM_TOTAL_MESSAGES = 2000;
 
 const styles = StyleSheet.create({
   container: {
@@ -30,12 +37,59 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  populateDBButton: {
+    height: 50,
+    width: 180,
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f9c99f'
+  }
 });
+
+console.disableYellowBox = true;
 
 export default class MyChat extends Component {
   async componentDidMount() {
-    const db = await SQLite.openDatabase({ name: 'test.db' });
-    await db.executeSql('CREATE TABLE IF NOT EXISTS `chat` ( `id` TEXT, `message` TEXT, `senderName` TEXT, `senderPicture` TEXT, PRIMARY KEY(`id`) );');
+    try {
+      this.db = await SQLite.openDatabase({ name: 'test.db' });
+      await this.db.executeSql('CREATE TABLE IF NOT EXISTS `chat` ( `id` TEXT, `message` TEXT, `senderName` TEXT, `senderPicture` TEXT, PRIMARY KEY(`id`) );');
+      const numChatsResult = await this.db.executeSql('select count(*) as numChats from chat;');
+      const numChats = numChatsResult[0].rows.item(0).numChats;
+      if (numChats < NUM_TOTAL_MESSAGES) {
+        this.populateDatabase();
+      }
+    } catch (error) {
+      console.log('Error is', error);
+    }
+  }
+
+  getRandomNumber = (min, max) => Math.floor(Math.random() * max) + min
+
+  populateDatabase = async () => {
+    try {
+      console.log('starting populatedb');
+      await this.db.executeSql('delete from chat;');
+      const response = await fetch('https://randomuser.me/api/?results=1000&inc=name,id,picture');
+      if (response.status < 200 || response.status >= 300) {
+        throw new Error('Got repsonse with status code: ' + response.status);
+      }
+      setTimeout(() => null, 0); // workaround for #issue-6679
+      const responseObject = await response.json();
+      const randomUsers = responseObject.results;
+      for (let index = 0; index < NUM_TOTAL_MESSAGES; index += 1) {
+        const randomUser = randomUsers[this.getRandomNumber(0, 1000)];
+        const id = uuid();
+        const senderName = `${randomUser.name.first} ${randomUser.name.last}`;
+        const senderPicture = randomUser.picture.medium;
+        const numberOfWords = this.getRandomNumber(10, 100);
+        const message = loremIpsum({ count: numberOfWords, units: 'words' });
+        await this.db.executeSql('insert into chat (id, message, senderName, senderPicture) values (?, ?, ?, ?)', [id, message, senderName, senderPicture]);
+      }
+      console.log('chats added successfully');
+    } catch (error) {
+      console.log('Error in populatedb', error);
+    }
   }
 
   render() {
@@ -51,6 +105,9 @@ export default class MyChat extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <TouchableOpacity style={styles.populateDBButton} onPress={this.populateDatabase}>
+          <Text>Repopulate Database</Text>
+        </TouchableOpacity>
       </View>
     );
   }
